e2dbcf4d9d5e8d6d5f0b1f29b9e0eda5d8ec92b5
Sanket Bhoir <139441397+Sanketbhoir03@users.noreply.github.com>
2025-09-24 11:39:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, World! This is my Flask App deployed with Jenkins!</h1>"
 
-# Run the app if this script is executed directly
+# In your app.py file
 if __name__ == '__main__':
-    # The host='0.0.0.0' makes the app publicly accessible on your network
     app.run(host='0.0.0.0', port=5000)
